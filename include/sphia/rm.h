b5d9a4563dbe3d0864944512f3c0c7b1d21e49dc
Stephen Mathieson <smath23@gmail.com>
2014-01-09 15:05:39 -0500

    Add sphia_rm
    Closes #9

---
+++ include/sphia/rm.h
@@ -0,0 +1,23 @@
+
+/**
+ * `rm.h' - libsphia
+ *
+ * Copyright (C) 2014 The libsphia Authors <sphia@googlegroups.com>
+ */
+
+#ifndef SPHIA_RM_H
+#define SPHIA_RM_H 1
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "common.h"
+#include "sphia.h"
+
+SPHIA_EXTERN int
+sphia_rm (sphia_t *, const char *);
+
+#ifdef __cplusplus
+}
+#endif
+#endif
---
+++ test/rm.c
@@ -0,0 +1,60 @@
+
+/**
+ * `test/rm.c' - libsphia
+ *
+ * Copyright (C) 2014 The libsphia Authors <sphia@googlegroups.com>
+ */
+
+#include <sphia/sphia.h>
+#include <sphia/new.h>
+#include <sphia/free.h>
+#include <sphia/set.h>
+#include <sphia/get.h>
+#include <sphia/rm.h>
+#include <sphia/test.h>
+
+TEST(simple_rm);
+
+TEST(rm) {
+  HEADER(sphia_rm);
+  RUN(simple_rm);
+  RUN(similar_keys_rm);
+  FOOTER();
+}
+
+TEST(simple_rm) {
+  sphia_t *sphia = sphia_new("./test-db");
+  assert(sphia);
+
+  assert(0 == sphia_set(sphia, "key", "value"));
+  assert(0 == sphia_rm(sphia, "key"));
+  assert(NULL == sphia_get(sphia, "key"));
+
+  sphia_free(sphia);
+  return 0;
+}
+
+TEST(similar_keys_rm) {
+  sphia_t *sphia = sphia_new("./test-db");
+  assert(sphia);
+
+  char *keys[] = {
+      "key"
+    , "key0"
+    , "key00"
+    , "key10"
+    , "key01"
+    , NULL
+  };
+
+  for (int i = 0; keys[i]; i++)
+    assert(0 == sphia_set(sphia, keys[i], "hello world!!"));
+
+  for (int i = 0; keys[i]; i++) {
+    assert(0 == sphia_rm(sphia, keys[i]));
+    assert(NULL == sphia_get(sphia, keys[i]));
+  }
+
+  sphia_free(sphia);
+  return 0;
+}
