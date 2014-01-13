b711007070f4a5061d62bb40710f0b85da81582e
Stephen Mathieson <smath23@gmail.com>
2014-01-13 10:26:03 -0500

    Allow clearing more 4096 keys

--- src/clear.c
+++ src/clear.c
@@ -11,34 +11,42 @@
 #include <sophia/sophia.h>
 #include <sphia/common.h>
 #include <sphia/rm.h>
+#include <sphia/count.h>
 #include <sphia/clear.h>
 
 int
 sphia_clear (sphia_t *self) {
   if (NULL == self) return -1;
 
+  // get number of keys in db
+  int size = sphia_count(self);
+  if (0 == size) return 0;
+
+  int index = 0;
+  // buffer for all keys
+  char *keys[size];
+
   void *c = sp_cursor(self->db, SPGT, NULL, 0);
   if (NULL == c) return -1;
 
-    // TODO remove magic number
-  char *keys[4096];
-  int count = 0;
-
+  // build array of keys
   while (sp_fetch(c)) {
     char *key = (char *) sp_key(c);
     if (key) {
-      count++;
-      keys[count] = key;
+      keys[index] = key;
+      index++;
     }
   }
 
   sp_destroy(c);
 
-  if (0 == count) return 0;
+  // not sure how we could end up here, but
+  // doesn't hurt to check
+  if (size != index) return -1;
+  if (0 == index) return 0;
 
-  for (int i = 1; i <= count; i++) {
-    char *key = keys[i];
-    if (-1 == sphia_rm(self, key)) return -1;
+  for (int i = 0; i < size; i++) {
+    if (-1 == sphia_rm(self, keys[i])) return -1;
   }
 
   return 0;
