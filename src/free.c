71ee8786f2de54763f7dc323e45ce46d59d0b79d
Stephen Mathieson <smath23@gmail.com>
2014-03-20 15:19:00 -0400

    Slight memory fix in sphia_free
    When destroying Sophia pointers, `db` must come first

--- src/free.c
+++ src/free.c
@@ -16,7 +16,7 @@
 
 void
 sphia_free (sphia_t *sphia) {
-  if (sphia->env) sp_destroy(sphia->env);
   if (sphia->db) sp_destroy(sphia->db);
+  if (sphia->env) sp_destroy(sphia->env);
   free(sphia);
 }
